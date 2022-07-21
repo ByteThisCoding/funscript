61a7b6db50aac3a949b4ed16f15ef911fefe5980
brandonqdixon <brandonqdixon@gmail.com>
2022-07-21 19:49:33 -0400

    Related Products Fix

--- src/equals/equals.ts
+++ src/equals/equals.ts
@@ -2,21 +2,29 @@ import { iEquals } from "../models/equals";
 
 export const Equals: iEquals = (a: any, b: any): boolean => {
     // If a and b reference the same value, return true
-    if (a === b) return true;
+    if (a === b) {
+        return true;
+    }
 
     // If a and b aren't the same type, return false
-    if (typeof a != typeof b) return false;
+    if (typeof a != typeof b) {
+        return false;
+    }
 
     // Already know types are the same, so if type is number
     // and both NaN, return true
-    if (typeof a == "number" && isNaN(a) && isNaN(b)) return true;
+    if (typeof a == "number" && isNaN(a) && isNaN(b)) {
+        return true;
+    }
 
     // Get internal [[Class]]
-    var aClass = Object.prototype.toString.call(a);
-    var bClass = Object.prototype.toString.call(b);
+    const aClass = Object.prototype.toString.call(a);
+    const bClass = Object.prototype.toString.call(b);
 
     // Return false if not same class
-    if (aClass != bClass) return false;
+    if (aClass != bClass) {
+        return false;
+    }
 
     // If they're Boolean, String or Number objects, check values
     if (
@@ -27,32 +35,44 @@ export const Equals: iEquals = (a: any, b: any): boolean => {
         return a.valueOf() == b.valueOf();
     }
 
-    if (aClass == "[object Date]") {
+    if (aClass === "[object Date]") {
         return +a == +b;
     }
 
     // If they're RegExps, Dates or Error objects, check stringified values
     if (
-        aClass == "[object RegExp]" ||
-        aClass == "[object Date]" ||
-        aClass == "[object Error]"
+        aClass === "[object RegExp]" ||
+        aClass === "[object Date]" ||
+        aClass === "[object Error]"
     ) {
-        return a.toString() == b.toString();
+        return a.toString() === b.toString();
+    }
+
+    // if it's a set or map, put to array or object
+    if (aClass === '[object Set]') {
+        a = [...a];
+        b = [...b];
+    } else if (aClass === '[object Map]') {
+        a = Object.fromEntries(a);
+        b = Object.fromEntries(b);
     }
 
     // Otherwise they're Objects, Functions or Arrays or some kind of host object
-    if (typeof a == "object" || typeof a == "function") {
+    if (typeof a === "object" || typeof a === "function") {
         // For functions, check stringigied values are the same
         // Almost certainly false if a and b aren't trivial
         // and are different functions
-        if (aClass == "[object Function]" && a.toString() != b.toString())
+        if (aClass === "[object Function]" && a.toString() !== b.toString()) {
             return false;
+        }
 
         var aKeys = Object.keys(a);
         var bKeys = Object.keys(b);
 
         // If they don't have the same number of keys, return false
-        if (aKeys.length != bKeys.length) return false;
+        if (aKeys.length !== bKeys.length) {
+            return false;
+        }
 
         // Check they have the same keys
         if (
